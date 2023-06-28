4b880eec89321c30d1382aebb3ac4a735deade07
sara niktabe <sara.niktabe@gmail.com>
2023-06-28 19:04:23 +0430

    complited

---
+++ Template Lierals/app.js
@@ -0,0 +1,17 @@
+let product1 = "pitzza";
+let price1 = 60;
+let product2 = "coca";
+let price2 = 20;
+let total = price1 + price2;
+
+let html1 = `
+<ul>
+<li>${product1}: ${price1}</li>
+<li>${product2}: ${price2}</li>
+<li>Total :${total}</li>
+</ul>
+
+`;
+
+let app = document.getElementById("addtemp");
+app.innerHTML = html1
