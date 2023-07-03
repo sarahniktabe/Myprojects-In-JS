a075b025e8a356dbee8548fdff73b4b55a1fdf09
sara niktabe <sara.niktabe@gmail.com>
2023-07-03 11:46:59 +0430

    app.js

--- Notepad LocalStorage/js/app.js
+++ Notepad LocalStorage/js/app.js
@@ -1,9 +1,23 @@
-//variabels
-
 //eventListeners
-document.querySelector("form").addEventListener("submit", newNote);
+var fn = document.querySelector("#form");
+fn.addEventListener("submit", newNote);
+var note = document.querySelector("#note");
+var noteList = document.querySelector("#note-list");
 
 //Functions
 function newNote(e) {
   e.preventDefault();
+
+  //remove btn
+  const removeBtn = document.createElement("a");
+  removeBtn.innerText = "X";
+  removeBtn.classList = "remove-note";
+
+  // add to notelist
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(note.Value));
+  li.appendChild(removeBtn);
+  noteList.appendChild(li);
+  console.log(note.Value);
+  //note.Value = "";
 }
