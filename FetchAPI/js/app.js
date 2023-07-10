00d2274aef1bb8679807deb7c258d814ed48f30b
sara niktabe <sara.niktabe@gmail.com>
2023-07-10 23:09:22 +0330

    done buttOn2

--- FetchAPI/js/app.js
+++ FetchAPI/js/app.js
@@ -1,9 +1,28 @@
 document.querySelector("#button1").addEventListener("click", loadData);
+document.querySelector("#button2").addEventListener("click", loadJSON);
 
 function loadData() {
   fetch("data.txt")
-  .then((response) => response.text())
-  .then((data)=>{
-    document.querySelector("#result").innerHTML = data;
-  })
+    .then((response) => response.text())
+    .then((data) => {
+      document.querySelector("#result").innerHTML = data;
+    });
+}
+
+function loadJSON() {
+  fetch("employees.json")
+    .then((response) => response.json())
+    .then((json) => {
+      //console.log(json)
+      let html = "";
+      json.forEach((person) => {
+        html += `
+           <li>
+           ${person.name} - ${person.job}
+           </li>
+           
+           `;
+        document.querySelector("#result").innerHTML = html;
+      });
+    });
 }
